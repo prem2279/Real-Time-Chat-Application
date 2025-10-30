72fea32442245a00976803b2dcbdebb5363e673d
prem.2279 <premgundu111@gmail.com>
2025-10-30 00:17:00 -0400

    Added render backend url in the setAllowed origins

--- chatapplication-backend/src/main/java/com/example/chatapplicationbackend/config/WebSocketConfig.java
+++ chatapplication-backend/src/main/java/com/example/chatapplicationbackend/config/WebSocketConfig.java
@@ -24,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
         registry.addEndpoint("/ws").
-                setAllowedOrigins("http://localhost:5173","http://localhost:3000","https://real-time-chat-application-backend-1kch.onrender.com").
+                setAllowedOrigins("http://localhost:5173","http://localhost:3000","https://real-time-chat-application-jk6g.onrender.com").
                 withSockJS();
     }
 }
